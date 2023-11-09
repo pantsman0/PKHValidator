1622e5a3ab615dfc85b56280e73bea2c5d2e26a3
woolf <woolf@RAMESH>
2023-11-09 22:41:03 +1030

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PokeHomeValidator
+{
+    partial class PokeHomeWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            richTextBox1 = new RichTextBox();
+            checkBox1 = new CheckBox();
+            textBox1 = new TextBox();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(491, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Validate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Location = new Point(3, 31);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(563, 399);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            checkBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(409, 6);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(76, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Recursive";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.BackColor = SystemColors.ButtonHighlight;
+            textBox1.Location = new Point(3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(400, 23);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "Click to select Pokemon Home dump folder";
+            textBox1.Click += textBox1_Click;
+            // 
+            // PokeHomeWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(569, 442);
+            Controls.Add(textBox1);
+            Controls.Add(checkBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button1);
+            Name = "PokeHomeWindow";
+            Text = "   ";
+            Load += PokeHomeWindow_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private CheckBox checkBox1;
+        private TextBox textBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+using System.DirectoryServices.ActiveDirectory;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PKHeX.Core;
+
+namespace PokeHomeValidator
+{
+    public partial class PokeHomeWindow : Form
+    { 
+        public PokeHomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PokeHomeWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SearchOption options = SearchOption.TopDirectoryOnly;
+            if (Directory.Exists(textBox1.Text))
+            {
+                if (checkBox1.Checked) { options = SearchOption.AllDirectories;}
+
+                List<String> pokemonFiles = Directory.EnumerateFiles(textBox1.Text, "*.ph3", options).ToList();
+
+                richTextBox1.AppendText($"Found {pokemonFiles.Count} PH3 files.\nProcessing...\n");
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+
+                var valid = new List<(string,string)>();
+                var invalid = new List<(string, string)>();
+
+                foreach (var pokeFile in  pokemonFiles)
+                {
+                    var (validity, message) = getValidityReport(pokeFile);
+                    if (validity)
+                    {
+                        valid.Add((pokeFile, message));
+                    } else
+                    {
+                        invalid.Add((pokeFile, message));
+                    }
+                }
+
+                richTextBox1.AppendText($"Validity report:\n\n Valid Pokemon Count: {valid.Count}\nInvalid Pokemon Count: {invalid.Count}\n\nInvalid Pokemon:\n");
+                foreach (var poke in invalid) { richTextBox1.AppendText($"{poke.Item1}:\n Message: {poke.Item2}\n\n"); }
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Unable to process directory: not found.");
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            };
+        }
+
+        private (bool, string) getValidityReport(string fileName)
+        {
+
+            try
+            {
+                PKH pk = new(File.ReadAllBytes(fileName));
+                PKM pkm; SaveFile savegame;
+                if (PersonalTable.SV.IsPresentInGame(pk.Species, pk.Form))
+                {
+                    savegame = new SAV9SV();
+                    pkm = pk.ConvertToPK9();
+
+                }
+                else if (PersonalTable.SWSH.IsPresentInGame(pk.Species, pk.Form))
+                {
+                    savegame = new SAV8SWSH();
+                    pkm = pk.ConvertToPK8();
+                }
+                else if (PersonalTable.BDSP.IsPresentInGame(pk.Species, pk.Form))
+                {
+                    savegame = new SAV8BS();
+                    pkm = pk.ConvertToPB8();
+                }
+                else if (PersonalTable.LA.IsPresentInGame(pk.Species, pk.Form))
+                {
+                    savegame = new SAV8LA();
+                    pkm = pk.ConvertToPA8();
+                }
+                else if (PersonalTable.GG.IsPresentInGame(pk.Species, pk.Form))
+                {
+                    savegame = new SAV7b();
+                    pkm = pk.ConvertToPB7();
+                }
+                else { return (false, $"Error: no valid switch game found for pokemon species {pk.Species}"); }
+
+
+
+                savegame.AdaptPKM(pkm);
+                if (pkm == null)
+                {
+                    return (false, "Error: unable to adapt pokemon to target game.");
+                }
+                var legality = new LegalityAnalysis(pkm);
+                if (legality.Valid) { return (true,"Valid!"); }
+                return (false, $"Error: Invalid Pokemon.\n Report: {legality.Report(false)} | GO pokemon: {pk.Met_Location == Locations.GO7 || pk.Met_Location == Locations.GO8} | My OT: {pk.OT_Name} ");
+            } catch (Exception ex) { return (false, $"Error: unable to process file.\nFileName: {fileName}\nError Message: {ex.Message}"); }
+        }
+    }
+}
